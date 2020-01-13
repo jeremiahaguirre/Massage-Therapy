8be1bc548ca44ea7a49dc13bd76b3d77b0c9ffb5
jeremiahaguirre <happytobike@gmail.com>
2020-01-13 15:38:54 -0800

    footer styling

--- themes/eduardoaguirre/footer.php
+++ themes/eduardoaguirre/footer.php
@@ -12,7 +12,8 @@
 
 <footer id="colophon" class="site-footer" role="contentinfo">
 	<div class="site-info">
-
+		<p>Email&#58; <a href="mailto:lookupnsee@yahoo.ca">mailto:lookupnsee@yahoo.ca</a></p>
+		<a href="https://www.facebook.com/eduardo.aguirre.7355" class="facebook"><i class="fab fa-facebook-f"></i></a>
 	</div><!-- .site-info -->
 </footer><!-- #colophon -->
 </div><!-- #page -->
