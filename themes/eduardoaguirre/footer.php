954aa6ee8d66334a118c9283d1d491c165bd5ca4
jeremiahaguirre <happytobike@gmail.com>
2020-01-17 14:18:04 -0800

    footer styling and page heirachy fix

--- themes/eduardoaguirre/footer.php
+++ themes/eduardoaguirre/footer.php
@@ -11,6 +11,9 @@
 </div><!-- #content -->
 
 <footer id="colophon" class="site-footer" role="contentinfo">
+	<div class="tel">
+		<p>Call: <a href="tel:+16049672920">604&ndash;967&ndash;2920</a></p>
+	</div>
 	<div class="site-info">
 		<?php echo do_shortcode('[DISPLAY_ULTIMATE_SOCIAL_ICONS]'); ?>
 	</div><!-- .site-info -->
